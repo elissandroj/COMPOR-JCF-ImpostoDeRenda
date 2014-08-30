e431cf30a599dee7c947ebd5ae363522f5401b58
aluno <aluno@labinfo03.cesed.local>
2014-08-30 12:04:18 -0300

    Tarefa T.01.04 finalizada

--- src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
+++ src/main/java/br/ufcg/ppgcc/compor/ir/impl/ImpostoDeRenda.java
@@ -3,6 +3,7 @@ package br.ufcg.ppgcc.compor.ir.impl;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.ufcg.ppgcc.compor.ir.ExcecaoImpostoDeRenda;
 import br.ufcg.ppgcc.compor.ir.FachadaExperimento;
 import br.ufcg.ppgcc.compor.ir.Titular;
 
@@ -10,7 +11,10 @@ public class ImpostoDeRenda implements FachadaExperimento {
 
 	private List<Titular> titulares = new ArrayList<Titular>();
 	
-	public void criarNovoTitular(Titular titular) {
+	public void criarNovoTitular(Titular titular) throws ExcecaoImpostoDeRenda {
+		if (titular.getNome() == null) {
+			throw new ExcecaoImpostoDeRenda("Nome Vazio");
+		}
 		titulares.add(titular);
 	}
 
